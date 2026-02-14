14f91941ccbeb6949f758beadaf03e06529d0fb8
momomak <m.makria@gmail.com>
2026-02-14 21:22:10 +0100

    db: creation migration for services

---
+++ database/migrations/1771099974767_create_services_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'services'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .notNullable()
+        .onDelete('CASCADE')
+      table.string('name').notNullable()
+      table.string('url').notNullable()
+      table.boolean('is_active').defaultTo(true).index()
+      table.timestamp('created_at').notNullable().defaultTo(this.now)
+      table.timestamp('updated_at').notNullable().defaultTo(this.now)
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
