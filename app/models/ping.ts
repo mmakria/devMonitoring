fc007aae326501c34cd2f4df2cfa5a530c227b7c
momomak <m.makria@gmail.com>
2026-02-14 21:57:05 +0100

    db: creation ping model

--- app/models/ping.ts
+++ app/models/ping.ts
@@ -1,9 +1,26 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Service from '#models/service'
 
 export default class Ping extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
+
+  @column()
+  declare serviceId: number
+
+  @column()
+  declare statusCode: number
+
+  @column()
+  declare responseTime: number
+
+  @column()
+  declare errorMessage: string | null
+
+  @belongsTo(() => Service)
+  declare service: BelongsTo<typeof Service>
 
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
